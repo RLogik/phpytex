8a9bb17012decd328c596f16b814ce694a62312e
RLogik <r.bitlogik@gmail.com>
2021-09-28 17:30:03 +0200

    dev > master: src - custom types indentation tracker reworked

--- src/customtypes/exports.py
+++ src/customtypes/exports.py
@@ -7,6 +7,6 @@
 
 from src.customtypes.type_configparameter import ConfigParameter;
 from src.customtypes.type_evaltype import EvalType;
+from src.customtypes.type_indentationtracker import IndentationTracker;
 from src.customtypes.type_projecttree import ProjectTree;
 from src.customtypes.type_transpileblock import TranspileBlock;
-from src.customtypes.type_indentationtracker import IndentationTracker;
--- src/customtypes/type_transpileblock.py
+++ src/customtypes/type_transpileblock.py
@@ -7,8 +7,10 @@
 
 from __future__ import annotations
 
+import re;
 from typing import Any;
 from typing import Dict;
+from typing import List;
 
 from src.core.utils import formatBlockIndent;
 
@@ -24,47 +26,84 @@ from src.core.utils import formatBlockIndent;
 
 class TranspileBlock(object):
     kind: str;
-    content: str;
-    num_lines: int;
+    _content: str;
+    lines: List[str];
     indentlevel: int;
-    indentchar: str;
+    indentsymb: str;
     parameters: Dict[str, Any];
     subst: Dict[str, TranspileBlock];
 
     def __init__(self,
         kind: str,
-        content: str = '',
+        content: Any = None,
+        lines: List[str] = [],
         indentlevel: int = 0,
-        indentchar: str = '    '
+        indentsymb: str = '    '
     ):
+        self.lines = lines;
         self.kind = kind;
         self.indentlevel = indentlevel;
-        self.indentchar = indentchar;
-        self.content = content;
+        self.indentsymb = indentsymb;
         self.parameters = dict();
         self.subst = dict();
+        if isinstance(content, str):
+            self._content = content;
         return;
 
+    @property
+    def content(self) -> str:
+        if hasattr(self, '_content'):
+            return self._content;
+        indent = self.indentsymb*self.indentlevel;
+        lines = formatBlockIndent(self.lines, indent=indent);
+        return '\n'.join(lines);
+
     def __str__(self) -> str:
+        indent = self.indentsymb*self.indentlevel;
         if self.kind == 'text':
             return self.content;
+        elif self.kind == 'text:linebreak':
+            return '{tab}print(\'\'\'\\n\'\'\');'.format(tab=indent);
         elif self.kind == 'text:subst':
             lines = [];
             lines.append('{tab}print(\'\'\'{expr}\'\'\'.format('.format(
-                tab  = self.indentchar*self.indentlevel,
+                tab  = indent,
                 expr = self.content,
             ));
             for key, value in self.subst.items():
-                value.indentlevel += 1;
+                indentlevel = value.indentlevel;
+                value_lines = formatBlockIndent(value.lines, indent=indent + self.indentsymb*2);
+                value_lines[0] = re.sub(r'\s*(.*)', r'\1', value_lines[0]);
                 lines.append('{tab}\'{key}\': {value},'.format(
-                    tab = self.indentchar*(self.indentlevel+1),
+                    tab = indent + self.indentsymb,
                     key = key,
-                    value = str(value),
+                    value = '\n'.join(value_lines),
                 ));
-                value.indentlevel -= 1;
+                value.indentlevel = indentlevel;
+            if len(self.subst) > 0:
+                lines.append('{tab});'.format(tab=indent));
+            else:
+                lines[0] += '));';
             return '\n'.join(lines);
         elif self.kind == 'code':
-            pass;
-        elif self.kind == 'code:inline':
-            return formatBlockIndent(self.content, indent=self.indentchar*self.indentlevel);
+            return self.content;
+        elif self.kind == 'code:value':
+            return self.content;
+        elif self.kind == 'code:set':
+            return '{tab}{varname} = {value};'.format(
+                tab = indent,
+                **self.parameters
+            );
+        elif self.kind == 'code:input':
+            return '';
+        elif self.kind == 'code:input:anon':
+            return '';
+        elif self.kind == 'code:bib':
+            return '';
+        elif self.kind == 'code:bib:anon':
+            return '';
+        elif self.kind == 'code:escape':
+            return '';
+        elif self.kind == 'code:escape:1':
+            return '';
         raise Exception('Unrecognised kind.');
