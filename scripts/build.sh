418c9cb8c0036616e80f7360c1ee4ec3be067c31
RLogik <r.bitlogik@gmail.com>
2021-10-09 13:02:49 +0200

    master > master: workflows --options flag made consistent with other flags

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,7 +17,7 @@ source scripts/.lib.sh;
 
 mode="$( get_one_kwarg_space "$SCRIPTARGS" "-+mode" "" )";
 lang="$( get_one_kwarg_space "$SCRIPTARGS" "-+lang" "python" )";
-options="$( get_kwarg "$SCRIPTARGS" "-+options" "" )";
+options="$( get_one_kwarg_space "$SCRIPTARGS" "-+options" "" )";
 option_venv="$( get_one_kwarg_space "$SCRIPTARGS" "-+venv" "true" )";
 ( $option_venv ) && use_python_venv_true || use_python_venv_false;
 
