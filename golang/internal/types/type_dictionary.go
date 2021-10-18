53e96419410cf2aba22fe00e0af62d1a451669f4
RLogik <r.bitlogik@gmail.com>
2021-10-18 19:08:37 +0200

    dev-go > master: src go - added dictionary type

---
+++ golang/internal/types/type_dictionary.go
@@ -0,0 +1,49 @@
+package types
+
+/* ---------------------------------------------------------------- *
+ * IMPORTS
+ * ---------------------------------------------------------------- */
+
+//
+
+/* ---------------------------------------------------------------- *
+ * TYPES
+ * ---------------------------------------------------------------- */
+
+type Dictionary struct {
+	values *map[string]interface{}
+}
+
+/* ---------------------------------------------------------------- *
+ * METHODS
+ * ---------------------------------------------------------------- */
+
+func (kw *Dictionary) Init() {
+	kw.values = nil
+}
+
+func (kw Dictionary) GetValues() *(map[string]interface{}) {
+	return kw.values
+}
+
+func (kw Dictionary) GetValue(key string) *interface{} {
+	var values = kw.GetValues()
+	if values != nil {
+		if value, ok := (*values)[key]; ok {
+			return &value
+		}
+	}
+	return nil
+}
+
+func (kw *Dictionary) SetValues(values *map[string]interface{}) {
+	kw.values = values
+}
+
+func (kw *Dictionary) SetValue(key string, value interface{}) {
+	var values = kw.GetValues()
+	if values != nil {
+		(*values)[key] = value
+	}
+	kw.values = values
+}
