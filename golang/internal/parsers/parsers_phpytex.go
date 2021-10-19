7cbd95c9595c9e65983f7029eabff72371d57a09
RLogik <r.bitlogik@gmail.com>
2021-10-19 13:00:47 +0200

    dev-go > master: src go - development of parser started

---
+++ golang/internal/parsers/parsers_phpytex.go
@@ -0,0 +1,31 @@
+package parsers
+
+/* ---------------------------------------------------------------- *
+ * IMPORTS
+ * ---------------------------------------------------------------- */
+
+import (
+	"phpytex/internal/types"
+)
+
+/* ---------------------------------------------------------------- *
+ * GLOBAL VARIABLES, CONSTANTS
+ * ---------------------------------------------------------------- */
+
+//
+
+/* ---------------------------------------------------------------- *
+ * METHODS
+ * ---------------------------------------------------------------- */
+
+func ParseText(
+	text string,
+	indentation types.IndentationTracker,
+	offset string,
+) ([]types.TranspileBlock, error) {
+	var err error = nil
+	var blocks []types.TranspileBlock
+	blocks = []types.TranspileBlock{}
+	// TODO
+	return blocks, err
+}
