d7e9f4b5a3ca75e73c11bbffbd65d0a01ac1703e
RLogik <r.bitlogik@gmail.com>
2021-10-19 07:24:09 +0200

    dev-go > master: src go - added util pipe call

--- golang/internal/core/utils/utils_pipe.go
+++ golang/internal/core/utils/utils_pipe.go
@@ -5,7 +5,12 @@ package utils
  * ---------------------------------------------------------------- */
 
 import (
+	"bufio"
+	"fmt"
+	"io"
+	"os/exec"
 	"runtime"
+	"strings"
 )
 
 /* ---------------------------------------------------------------- *
@@ -14,4 +19,33 @@ import (
 
 func IsLinux() bool {
 	return !(runtime.GOOS == "windows")
+}
+
+func PipeCall(name string, parts ...string) error {
+	var (
+		err    error
+		cmd    *exec.Cmd
+		pipe   io.ReadCloser
+		stream *bufio.Reader
+		line   []byte
+	)
+	cmd = exec.Command(name, parts...)
+	pipe, err = cmd.StdoutPipe()
+	if err == nil {
+		err = cmd.Start()
+		stream = bufio.NewReader(pipe)
+	}
+	for err == nil {
+		line, _, err = stream.ReadLine()
+		if err == nil {
+			fmt.Println(string(line))
+		}
+	}
+	if err != nil && err != io.EOF {
+		return fmt.Errorf(
+			"Shell command < \033[94;1m%[1]s\033[0m > failed.",
+			strings.Join(append([]string{name}, parts...), " "),
+		)
+	}
+	return nil
 }
