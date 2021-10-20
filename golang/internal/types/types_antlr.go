0515c033fc93aa5a9dc43bb9e08b4e6c4b53760d
RLogik <r.bitlogik@gmail.com>
2021-10-20 10:30:49 +0200

    dev-go > master: src go - added tree type for parser + serialisation

--- golang/internal/types/types_antlr.go
+++ golang/internal/types/types_antlr.go
@@ -12,36 +12,106 @@ import (
  * TYPES
  * ---------------------------------------------------------------- */
 
+type Ant struct {
+	tree   antlr.Tree
+	parser *antlr.Parser
+}
+
 type AntlrTree struct {
-	Tree   antlr.Tree
-	Parser *antlr.Parser
+	Terminal bool
+	Kind     string
+	Value    *string
+	ant      *Ant
+	children *[]AntlrTree
 }
 
 /* ---------------------------------------------------------------- *
  * METHODS for types
  * ---------------------------------------------------------------- */
 
-func (self AntlrTree) GetChildren() []AntlrTree {
-	var nodes = self.Tree.GetChildren()
-	var subants = make([]AntlrTree, len(nodes))
+func NewAnt(tree antlr.Tree, parser antlr.Parser) Ant {
+	return Ant{tree: tree, parser: &parser}
+}
+
+func NewAntlrTree(tree antlr.Tree, parser antlr.Parser) AntlrTree {
+	var result Ant = NewAnt(tree, parser)
+	return result.ToTree()
+}
+
+func (self Ant) ToTree() AntlrTree {
+	var tree AntlrTree
+	if self.IsTerminal() {
+		tree = AntlrTree{
+			Terminal: true,
+			Kind:     "TERMINAL",
+			Value:    StringToPtr(self.GetText()),
+			ant:      &self,
+			children: nil,
+		}
+	} else {
+		tree = AntlrTree{
+			Terminal: false,
+			Kind:     self.GetText(),
+			Value:    nil,
+			ant:      &self,
+			children: nil,
+		}
+	}
+	var children []AntlrTree
+	var nodes []Ant = self.GetChildren()
+	children = make([]AntlrTree, len(nodes))
+	for i, subant := range nodes {
+		children[i] = subant.ToTree()
+	}
+	if len(children) > 0 {
+		tree.children = &children
+	}
+	return tree
+}
+
+func (self Ant) GetChildren() []Ant {
+	var nodes = self.tree.GetChildren()
+	var subants = make([]Ant, len(nodes))
 	for i, node := range nodes {
-		subants[i] = AntlrTree{Tree: node, Parser: self.Parser}
+		subants[i] = Ant{tree: node, parser: self.parser}
 	}
 	return subants
 }
 
-func (self AntlrTree) GetLabel() string {
-	return antlr.TreesGetNodeText(self.Tree, []string{}, *self.Parser)
+func (self AntlrTree) GetChildren() []AntlrTree {
+	if self.children == nil {
+		return []AntlrTree{}
+	}
+	return *self.children
 }
 
-func (self AntlrTree) GetTextContent() string {
+func (self Ant) IsTerminal() bool {
+	return (len(self.tree.GetChildren()) == 0)
+}
+
+func (self AntlrTree) IsTerminal() bool {
+	return self.children == nil
+}
+
+func (self Ant) GetText() string {
+	return antlr.TreesGetNodeText(self.tree, []string{}, *self.parser)
+}
+
+func (self Ant) GetTextContent() string {
 	var expr string = ""
 	var subants = self.GetChildren()
-	if len(subants) == 0 {
-		return self.GetLabel()
+	if self.IsTerminal() {
+		return self.GetText()
 	}
 	for _, subant := range subants {
 		expr += subant.GetTextContent()
 	}
 	return expr
+}
+
+func (self AntlrTree) GetTextContent() string {
+	if self.ant == nil {
+		return ""
+	}
+	return (*self.ant).GetTextContent()
 }
---
+++ golang/internal/types/types_serialise_antlr.go
@@ -0,0 +1,77 @@
+package types
+
+/* ---------------------------------------------------------------- *
+ * IMPORTS
+ * ---------------------------------------------------------------- */
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+)
+
+/* ---------------------------------------------------------------- *
+ * METHODS serialisation of antlr types
+ * ---------------------------------------------------------------- */
+
+func (self AntlrTree) Serialise() map[string]interface{} {
+	var serialised = map[string]interface{}{
+		"Kind":     self.Kind,
+		"Value":    self.Value,
+		"children": nil,
+	}
+	var subtrees = self.GetChildren()
+	if len(subtrees) > 0 {
+		var children = make([]map[string]interface{}, len(subtrees))
+		for i, subtree := range subtrees {
+			children[i] = subtree.Serialise()
+		}
+	}
+	serialised["children"] = subtrees
+	return serialised
+}
+
+func (self AntlrTree) String() string {
+	var ch chan string
+	var wg *sync.WaitGroup = &sync.WaitGroup{}
+	var lines []string
+	ch = make(chan string)
+	wg.Add(1)
+	go self.stringify(0, "", "  ", " |__", wg, ch)
+	go (func() {
+		wg.Wait()
+		close(ch)
+	})()
+	lines = []string{}
+	for line := range ch {
+		lines = append(lines, line)
+	}
+	return strings.Join(lines, "\n")
+}
+
+func (self AntlrTree) stringify(
+	depth int,
+	prefix string,
+	tab string,
+	branch string,
+	wg *sync.WaitGroup,
+	ch chan string,
+) {
+	defer wg.Done()
+	var indent string
+	var line string
+	indent = prefix
+	if depth > 0 {
+		indent += strings.Repeat(tab, depth-1) + branch
+	}
+	if self.Terminal {
+		line = fmt.Sprintf(`%[1]s %[2]s`, indent, PtrToString(self.Value, "<null>"))
+	} else {
+		line = fmt.Sprintf(`%[1]s %[2]s:`, indent, self.Kind)
+	}
+	ch <- line
+	for _, subtree := range self.GetChildren() {
+		wg.Add(1)
+		go subtree.stringify(depth+1, prefix, tab, branch, wg, ch)
+	}
+}
