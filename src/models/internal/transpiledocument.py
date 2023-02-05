4519115b2f125f4e2754221f5ab5a1855f56c80a
RLogik <rlogik@users.noreply.github.com>
2023-02-05 11:17:03 +0100

    tmp-under-construction > main: further work

--- src/models/internal/transpiledocument.py
+++ src/models/internal/transpiledocument.py
@@ -10,6 +10,7 @@ from src.thirdparty.system import *;
 from src.thirdparty.types import *;
 
 from src.core.utils import *;
+from src.models.generated.tokenisation import *;
 from src.models.internal.transpileblock import *;
 from src.models.internal.transpileblocks import *;
 
@@ -25,20 +26,13 @@ __all__ = [
 # CLASS transpile document
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-@dataclass
-class TranspileDocumentRaw:
-    label: str = field();
-    root: str = field();
-    path: str = field();
-    indentsymb: str = field();
-    variables: dict[str, Any] = field(default_factory=dict);
-    pathfolder: str = field(init=False)
-    blocks: TranspileBlocks = field(init=False, default_factory=TranspileBlocks);
+class TranspileDocument(TokenisationDocument):
+    blocks: TranspileBlocks;
+    path_folder: str;
 
-class TranspileDocument(TranspileDocumentRaw):
     def __init__(self, *_, **__):
-        super(TranspileDocument, self).__init__(*_, **__);
-        self.pathfolder = os.path.dirname(self.path) or '.';
+        super(TokenisationDocument, self).__init__(*_, **__);
+        self.path_folder = os.path.dirname(self.path) or '.';
         return;
 
     def __len__(self) -> int:
@@ -49,7 +43,7 @@ class TranspileDocument(TranspileDocumentRaw):
             yield block;
 
     def tab(self, offset: int = 1) -> str:
-        return self.indentsymb * offset;
+        return self.indent_symbol * offset;
 
     # relativises a path relative to directory to a path relative to root
     def relativisePath(self, path: str):
@@ -57,7 +51,7 @@ class TranspileDocument(TranspileDocumentRaw):
             if path.startswith(self.root):
                 path = os.path.relpath(path=path, start=self.root);
         else:
-            path = os.path.join(self.pathfolder, path);
+            path = os.path.join(self.path_folder, path);
             path = os.path.relpath(path=path, start=self.root);
         return path;
 
@@ -72,14 +66,9 @@ class TranspileDocument(TranspileDocumentRaw):
         anon:       bool      = False,
         hide:       bool      = False,
     ) -> Generator[str, None, None]:
-        yield '{tab}# generate content from file \'{path}\''.format(
-            tab  = self.tab(offset),
-            path = self.path,
-        );
-        yield '{tab}def {label}():'.format(
-            tab  = self.tab(offset),
-            label = self.label,
-        );
+        yield f'{self.tab(offset)}# generate content from file \'{self.path}\'';
+        yield f'{self.tab(offset)}def {self.label}():';
+        offset += 1;
         yield from TranspileBlock(
             kind = 'code',
             lines = [
@@ -87,17 +76,17 @@ class TranspileDocument(TranspileDocumentRaw):
                 for name in unique([ '__ROOT__', '__DIR__', '__FNAME__', '__ANON__', '__HIDE__', '__IGNORE__' ] + globalvars)
                 if not name in [ '__STATE__' ]
             ] + [
-                '__ROOT__ = \'.\';'.format(),
-                '__DIR__ = \'{path}\';'.format(path = self.pathfolder),
-                '__FNAME__ = \'{path}\';'.format(path = self.path),
+                '__ROOT__ = \'.\';',
+                f'__DIR__ = \'{self.path_folder}\';',
+                f'__FNAME__ = \'{self.path}\';',
                 '__IGNORE__ = False;',
-                '__ANON__ = {};'.format(anon),
-                '__HIDE__ = {};'.format(hide),
+                f'__ANON__ = {anon};',
+                f'__HIDE__ = {hide};',
                 '# Save current state locally. Use to restore state after importing subfiles.',
                 '__STATE__ = (__ROOT__, __DIR__, __FNAME__, __ANON__, __HIDE__, __IGNORE__);',
             ]
-        ).generateCode(offset + 1, anon=False, hide=False);
+        ).generateCode(offset, anon=False, hide=False);
         for block in self.blocks:
-            yield from block.generateCode(offset + 1, anon=anon, hide=hide);
-        yield '{tab}return;'.format(tab=self.tab(offset + 1));
+            yield from block.generateCode(offset, anon=anon, hide=hide);
+        yield f'{self.tab(offset)}return;';
         return;
